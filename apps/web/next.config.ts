9f882340681ce247e8c11057c3bca601b853e595
Luiz Castro <luizcastro2122@gmail.com>
2026-01-22 19:44:27 -0300

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@beroboard/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,23 @@
+import prisma from "@beroboard/db";
+import { env } from "@beroboard/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "mongodb",
+  }),
+
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
