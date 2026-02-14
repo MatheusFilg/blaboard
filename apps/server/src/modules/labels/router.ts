9675ab824ca5e315c20363eb4fe5f78f60169eb6
Matheus F <matheus_falmeida@hotmail.com>
2026-02-14 12:07:02 -0300

    chore: remove assign and remove label routes

--- apps/server/src/modules/labels/router.ts
+++ apps/server/src/modules/labels/router.ts
@@ -3,8 +3,6 @@ import { createLabelRouter } from "./create-label/router";
 import { getLabelsRouter } from "./get-labels/router";
 import { updateLabelRouter } from "./update-label/router";
 import { deleteLabelRouter } from "./delete-label/router";
-import { assignLabelRouter } from "./assign-label/router";
-import { removeLabelRouter } from "./remove-label/router";
 
 export const labelsRouter = new Elysia({
 	prefix: "/labels",
@@ -14,6 +12,4 @@ export const labelsRouter = new Elysia({
   getLabelsRouter,
   updateLabelRouter,
   deleteLabelRouter,
-  assignLabelRouter,
-  removeLabelRouter,
 ]);
